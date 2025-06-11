1469474629234b80500348591375f13a5b08c113
DaveToTaxi <taxismad@gmail.com>
2025-06-11 11:23:30 +0200

    feat: LCP y optimizaciones generales para móvil con Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/static';
+import image from '@astrojs/image';
+import critical from 'astro-critical-css';
+// import unpic from '@unpic/astro';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
+  integrations: [
+    image({
+      service: 'vercel-image-optimization'
+    }),
+    critical(),
+    // unpic({
+    //   placeholder: 'dominantColor',
+    //   layout: 'constrained'
+    // }),
+    tailwind()
+  ],
 });
